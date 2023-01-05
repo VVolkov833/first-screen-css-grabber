dbfb35e1f2f29c9a05153846f005c5019fae3a1a
VVolkov833 <finnish.ru@gmail.com>
2023-01-05 14:28:49 +0100

    elements retreiving ready, processing

--- main.js
+++ main.js
@@ -1,55 +1,54 @@
-// ++ test cases for different url variants / a ./ ../ http://
-// ++ test cases for recoursive media with different url variants
-// ++ mention @import can behave like @media
+// ++ mention @import can behave like @media el.media.mediaText
+const stylesStructure = async (el) => {
 
-const getContent = async url => {
-    const getPath = url => {
-        const doc = document.implementation.createHTMLDocument( '' ),
-              a = doc.createElement( 'a' );
-        a.href = url;
-        return a.href.replace( a.hash, '' ).replace( a.search, '' ).replace( /([^\/])\/[^\.\/]+(?:\.[^\.\/]+)+$/, '$1/' ); // hash, query, filename
-    };
-    const path = getPath( url );
-    const fixUrls = content => {
-        let new_content = content;
-        new_content = new_content.replace( /url\(('|")?(https?\:\/\/|\/|\/\/|data\:)?(.{10})/gi, (m, m1, m2, m3) => { // ++remove m3 & logs
-            //console.log( m );
-            if ( m2 ) { return m }
-            //console.log( '^this to this v' );
-            //console.log( 'url(' + (m1||'') + path + m3 );
-            //console.log( path );
-            return 'url(' + (m1||'') + path + m3;
-        });
-        new_content = new_content.replace( /\@import\s*('|")(https?\:\/\/|\/|\/\/|data\:)?(.{10})/gi, (m, m1, m2, m3) => { // ++remove m3 & logs
-            //console.log( m );
-            if ( m2 ) { return m }
-            //console.log( '^this to this v' );
-            //console.log( '@import ' + (m1||'') + path + m3 );
-            //console.log( path );
-            return '@import ' + (m1||'') + path + m3;
-        });
-        return new_content;
-    };
-    const response = await fetch( url );
-    if ( !response.ok ) { return ''; }
-    return await fixUrls( await response.text() );
-}
-const parseCSS = s => {
-    const doc = document.implementation.createHTMLDocument( '' ),
-          style = doc.createElement( 'style' );
-    style.textContent = s;
-    doc.body.appendChild( style );
-    return style.sheet.cssRules;
-};
-const importStyles = async ( parsed ) => {
-    let style = [];
-    for ( let rule of parsed ) {
-        if ( rule.constructor.name !== 'CSSImportRule' ) { break } // @import can only be at the start of a css document
-        style.push( await styleStructure( rule ) );
+    const getContent = async url => {
+        const getPath = url => {
+            const doc = document.implementation.createHTMLDocument( '' ),
+                  a = doc.createElement( 'a' );
+            a.href = url;
+            return a.href.replace( a.hash, '' ).replace( a.search, '' ).replace( /([^\/])\/[^\.\/]+(?:\.[^\.\/]+)+$/, '$1/' ); // hash, query, filename
+        };
+        const path = getPath( url );
+        const fixUrls = content => {
+            let new_content = content;
+            new_content = new_content.replace( /url\(('|")?(https?\:\/\/|\/|\/\/|data\:)?(.{10})/gi, (m, m1, m2, m3) => { // ++remove m3 & logs
+                //console.log( m );
+                if ( m2 ) { return m }
+                //console.log( '^this to this v' );
+                //console.log( 'url(' + (m1||'') + path + m3 );
+                //console.log( path );
+                return 'url(' + (m1||'') + path + m3;
+            });
+            new_content = new_content.replace( /\@import\s*('|")(https?\:\/\/|\/|\/\/|data\:)?(.{10})/gi, (m, m1, m2, m3) => { // ++remove m3 & logs
+                //console.log( m );
+                if ( m2 ) { return m }
+                //console.log( '^this to this v' );
+                //console.log( '@import ' + (m1||'') + path + m3 );
+                //console.log( path );
+                return '@import ' + (m1||'') + path + m3;
+            });
+            return new_content;
+        };
+        const response = await fetch( url );
+        if ( !response.ok ) { return ''; }
+        return await fixUrls( await response.text() );
     }
-    return style;
-};
-const styleStructure = async (el) => {
+
+    const importStyles = async ( parsed ) => {
+        let style = [];
+        for ( let rule of parsed ) {
+            if ( rule.constructor.name !== 'CSSImportRule' ) { break } // @import can only be at the start of a css document
+            style.push( await stylesStructure( rule ) );
+        }
+        return style;
+    };
+    const parseCSS = s => {
+        const doc = document.implementation.createHTMLDocument( '' ),
+              style = doc.createElement( 'style' );
+        style.textContent = s;
+        doc.body.appendChild( style );
+        return style.sheet.cssRules;
+    };
     const url = el?.href || window.location.href,
           tag = el?.tagName?.toLowerCase() || '@import',
           content = el?.textContent || await getContent( url );
@@ -62,75 +61,142 @@ const styleStructure = async (el) => {
     };
 
     result.imported = await importStyles( result.parsed ); // @import
-    // ++media value before parsing
 
     return result;
 };
 
+const allStyles = await Promise.all( [...document.querySelectorAll( 'link[rel=stylesheet], style' )].map( stylesStructure ) );
 
-let allStyles = []; // contains all tags providing styling
-document.querySelectorAll( 'link[rel=stylesheet], style' ).forEach( ( el ) => {
-    allStyles.push( el );
-});
-allStyles = await Promise.all( allStyles.map( styleStructure ) );
-
-const structurePrint = structure => {
-    let store = [];
+const structureShow = structure => {
+    let lvl = 0;
     const imported = rules => {
-        let store = [];
-        if ( !rules.imported.length ) { return undefined }
-        for ( let rule in rules.imported ) {
-            store[rule] = [
-                rules.imported[rule].tag+' '+rules.imported[rule].url+' '+rules.imported[rule].el.media.mediaText,
-                imported( rules.imported[rule] ) || undefined
-            ];
+        if ( !rules.imported.length ) { return }
+        lvl++;
+        for ( let rule of rules.imported ) {
+            console.log( '\t '.repeat(lvl)+'▶'+rule.tag+' '+rule.url );
+            imported( rule );
         }
-        return store;
+        lvl--;
     };
-    for ( let rule in structure ) {
-        store[rule] = [
-            structure[rule].el.tagName.toLowerCase()+(structure[rule].el.id?'#'+structure[rule].el.id:''),
-            imported( structure[rule] ) || undefined // import goes after only to show the structure, the imported content goes before the parent style
-        ];
+    for ( let rule of structure ) {
+        console.log( '▶'+rule.el.tagName.toLowerCase()+(rule.el.id?'#'+rule.el.id:'')+(rule.tag==='link'?' '+rule.url:'') );
+        imported( rule ); // import goes after only to show the structure, the imported content goes before the parent style
     }
-    return store;
 };
 
-console.log( structurePrint( allStyles ) );
-throw '';
+//structureShow( allStyles );
+//throw '';
 
+const dom = (() => { //!!++test on fixed elements, especially, borlabs
 
-
-const getFirstScreenElements = () => {
+    const onFirstScreen = el => {
+        const r = el.getBoundingClientRect();
+        return (
+            r.top + scroll.top < win.height &&
+            r.left + scroll.left < win.width
+            //&&
+            //r.bottom + scroll.top > 0 &&
+            //r.right + scroll.left > 0
+        );
+    };
 
     const win = {
         width: ( window.innerWidth || document.documentElement.clientWidth ) + 1,
         height: ( window.innerHeight || document.documentElement.clientHeight ) + 1
     };
     win.height = Math.round( win.height * 1.1 );
+    const scroll = { top: document.documentElement.scrollTop, left: document.documentElement.scrollLeft };
 
-    const onFirstScreen = el => {
-        const r = el.getBoundingClientRect();
-        const s = { top: document.documentElement.scrollTop, left: document.documentElement.scrollLeft };
-        return (
-            r.top + s.top < win.height &&
-            //r.bottom + s.top > 0 &&
-            r.left + s.left < win.width //&&
-            //r.right + s.left > 0
-        );
+    let dom = { all : [], first : [], rest : [] }; //++-- rest not needed actually?
+    document.body.querySelectorAll( '*' ).forEach( el => {
+        dom.all.push( el );
+        ( onFirstScreen( el ) ? dom.first : dom.rest ).push( el );
+    });
+
+    dom.all.push( document.documentElement );
+    dom.all.push( document.body );
+    dom.first.push( document.documentElement );
+    dom.first.push( document.body );
+
+    return dom;
+})();
+
+//console.log( dom );
+//throw '';
+
+const stylesBundle = structure => {
+    // ++split / not split a selector - add to settings
+    // ++try-catch if selector is valid after cleaning with logging in the console
+    const bundle = { first : '', rest : '', unused : '', randu : '',
+        wrap : function(content, wrapper) { // ++just make it not iterable
+            if ( !content ) { return {} }
+            let result = {};
+            for ( let key in this ) {
+                if ( typeof this[key] !== 'string' && this[key] !instanceof String ) { continue }
+                result[key] = wrapper + '{' + content + '}';
+            }
+            return result;
+        },
+        add : function(rules = {}) {
+            for ( let key in this ) {
+                if ( typeof this[key] !== 'string' && this[key] !instanceof String ) { continue }
+                this.key += rules;
+            }
+        }
     };
 
-    let firstScreenElements = [];
-    document.body.querySelectorAll( '*' ).forEach( el => {
-        if ( !onFirstScreen( el ) ) { return; }
-        firstScreenElements.push( el );
-    });
-    firstScreenElements.push( document.documentElement );
-    firstScreenElements.push( document.body );
+    const proceed = rule => {
+        const name = rule.constructor.name;
+        if ( name === 'CSSMediaRule' ) {
+            const result = proceed( value.cssRules );
+            firstScreenCSS += result ? '@media ' + value.conditionText + '{' + result + '}' : '';
+            return;
+        }
+        if ( name === 'CSSSupportsRule' ) {
+            const result = proceed( value.cssRules );
+            firstScreenCSS += result ? '@supports ' + value.conditionText + '{' + result + '}' : '';
+            return;
+        }
+        if ( name === 'CSSFontFaceRule' ) {
+            css.firstScreenCSS += value.cssText;
+            return;
+        }
+        if ( name !== 'CSSStyleRule' ) {
+            // console.error( 'Not used rule ' + name ); console.log( rule );
+            // ignore CSSImportRule, as it is already retreived and parsed
+            return;
+        }
 
-    return firstScreenElements;
+/*
+        const clearSelector = value.selectorText
+            .replace( /\s:{1,2}(?:before|after)/gi, ' *' ) // .class ::before -> .class *
+            .replace( /:{1,2}(?:before|after|focus\-within|focus\-visible|first\-letter|focus|hover|active|target|visited)/gi, '' ) // .class::before -> .class
+            .replace( /:not\(\)/, '' ) // :not(:focus)
+            .replace(/^[\,\s]+|[\,\s]+$/g, ''); // , .class,
+        try { // in case there are broken selectors after clearing..
+            document.querySelector( clearSelector );
+        } catch { return }
+
+        const elements = document.querySelectorAll( clearSelector );
+
+        const isInScreen = elements.length || false; // ++?? can return here
+        const isInFirstScreen = Array.prototype.some.call( elements, el => { return firstScreenElements.includes( el ) });
+//*/
+    };
+    const imported = rules => {
+        if ( !rules.imported.length ) { return }
+        for ( let rule of rules.imported ) {
+            console.log( '\t '.repeat(lvl)+'▶'+rule.tag+' '+rule.url );
+            imported( rule );
+        }
+    };
+    for ( let rules of structure ) {
+        imported( rule );
+        console.log( '▶'+rule.el.tagName.toLowerCase()+(rule.el.id?'#'+rule.el.id:'')+(rule.tag==='link'?' '+rule.url:'') );
+    }
 };
-let firstScreenElements = getFirstScreenElements();
+
+throw '';
 
 const filterVisibleCSS = () => {
 
@@ -264,14 +330,10 @@ printStyles( 'Unused CSS', unusedCSS );
 printStyles( 'Rest Screen and Unused CSS', restAndUnusedCSS );
 
 
-// make the flat tree of styles first?
-
-// found one undefined for the url base on https://plastische-chirurgie-giessler.de/home/leistungen/brust/bruststraffung/ with logo-giessler-sticky.png
 // ++beautify
 
 // ++add the list to pick which styles to proceed
-// ++mention @import
 
 // ++should also separate selectors by , for smaller first screen and check each separately, but that will cause doubling of attributes on the rest screen
 // ++exclude :focus, :hover and others from the first screen
-// ++borlabs overflow (position:fixed) goes to second screen
+// ++Yborlabs overflow (position:fixed) goes to second screen
